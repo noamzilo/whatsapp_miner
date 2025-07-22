a8dcf390cc1b7c95edf4a1af067e152309ab32a5
noamAsusWSL <noam@plai.live>
2025-07-22 16:25:22 -0500

    sanitized variables, and now auth works with greenapi

--- src/env_var_injection.py
+++ src/env_var_injection.py
@@ -1,4 +1,8 @@
 import os
 
-instance_id = os.getenv("GREEN_API_INSTANCE_ID")
-api_token = os.getenv("GREEN_API_INSTANCE_API_TOKEN")
+def sanitize_env_var(env_var):
+	env_var = env_var.replace('"', "")
+	return env_var
+
+instance_id = sanitize_env_var(os.getenv("GREEN_API_INSTANCE_ID"))
+api_token = sanitize_env_var(os.getenv("GREEN_API_INSTANCE_API_TOKEN"))
