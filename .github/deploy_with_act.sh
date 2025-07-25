feccec8ae794a9174dd8a714836959beae3fd1a5
noamAsusWSL <noam@plai.live>
2025-07-25 09:25:17 -0500

    act seems to actually do something now

--- .github/deploy_with_act.sh
+++ .github/deploy_with_act.sh
@@ -2,6 +2,10 @@
 set -euo pipefail
 
 USER_ID=$(id -u)
+GROUP_ID=$(getent group docker | cut -d: -f3)
+
 doppler secrets download --no-file --format env \
 	| sed 's/^/-s /' \
-	| xargs act --container-options "-u $USER_ID" -W .github/workflows/deploy.yml
+	| xargs act \
+		--container-options "-u $USER_ID:$GROUP_ID" \
+		-W .github/workflows/deploy.yml
